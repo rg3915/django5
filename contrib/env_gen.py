b4afb93423b56f4a807c29cf7420c47f898a6e1b
rg3915 <regis42santos@gmail.com>
2023-12-23 12:45:04 -0300

    Corrige env_gen.py

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -7,29 +7,14 @@ chars = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%^&
 size = 50
 secret_key = "".join(random.sample(chars, size))
 
-chars = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%_"
-size = 20
-password = "".join(random.sample(chars, size))
-
 CONFIG_STRING = """
-DEBUG=True
 SECRET_KEY=%s
-ALLOWED_HOSTS=127.0.0.1,.localhost,0.0.0.0
-
-#DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
-#POSTGRES_DB=
-#POSTGRES_USER=
-#POSTGRES_PASSWORD=%s
-#DB_HOST=localhost
-
-#DEFAULT_FROM_EMAIL=
-#EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-#EMAIL_HOST=localhost
-#EMAIL_PORT=
-#EMAIL_HOST_USER=
-#EMAIL_HOST_PASSWORD=
-#EMAIL_USE_TLS=True
-""".strip() % (secret_key, password)
+POSTGRES_DB=dicas_de_django_db
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+DB_HOST=localhost
+DB_PORT=5431
+""".strip() % secret_key
 
 # Writing our configuration file to '.env'
 with open('.env', 'w') as configfile:
